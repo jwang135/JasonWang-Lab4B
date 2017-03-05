041dabf26ac7b71b714848e604a22ce134cf9535
Jason Wang <wangjason@wustl.edu>
2017-03-05 17:30:14 -0600

    Initial Commit

---
+++ JasonWang-Lab4BTests/JasonWang_Lab4BTests.swift
@@ -0,0 +1,36 @@
+//
+//  JasonWang_Lab4BTests.swift
+//  JasonWang-Lab4BTests
+//
+//  Created by J Wang on 3/5/17.
+//  Copyright © 2017 J Wang. All rights reserved.
+//
+
+import XCTest
+@testable import JasonWang_Lab4B
+
+class JasonWang_Lab4BTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
